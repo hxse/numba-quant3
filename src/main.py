865ec9937b7780fedb50e0060317881888d94e6e
hxse <qmlibulqxk@gmail.com>
2025-08-18 11:17:23 +0800

    update

--- src/main.py
+++ src/main.py
@@ -30,8 +30,20 @@ def main(
     set_numba_dtypes(enable64=enable64, cache=cache)
     print(f"cache from cli: {cache}")
 
+    cache = numba_config["cache"]
+    nb_float = numba_config["nb"]["float"]
+    np_float = numba_config["np"]["float"]
+
     # 在解析参数并更新全局配置字典后，再导入 Numba 函数
-    from params import get_params_template
+    from params import (
+        create_params_list_template,
+        create_params_dict_template,
+        get_params_list_value,
+        set_params_list_value,
+        get_params_dict_value,
+        set_params_dict_value,
+        convert_params_dict_list,
+    )
     from utils.mock_data import get_mock_data
     from parallel import parallel_entry
 
@@ -43,7 +55,26 @@ def main(
 
     tohlcv_np = get_mock_data(data_count=3)
 
-    (indicator_params_list, backtest_params_list) = get_params_template(params_count=2)
+    (indicator_params_list, backtest_params_list) = create_params_list_template(
+        params_count=2
+    )
+    np_arr = get_params_list_value("sma_period", indicator_params_list)
+    indicator_params_list = set_params_list_value(
+        "sma_period", indicator_params_list, np.array([1, 2], dtype=np_float)
+    )
+
+    (indicator_params_dict, backtest_params_dict) = create_params_dict_template(
+        params_count=2
+    )
+    _indicator_params_list = convert_params_dict_list(indicator_params_dict)
+
+    _indicator_params_dict = get_params_dict_value("sma_period", indicator_params_dict)
+    _indicator_params_dict2 = set_params_dict_value(
+        "sma_period", indicator_params_dict, np.array([1, 2], dtype=np_float)
+    )
+    import pdb
+
+    pdb.set_trace()
 
     # 记录参数生成结束时间，并计算运行时间
     if show_timing:
--- src/params.py
+++ src/params.py
@@ -7,48 +7,38 @@ from numba.typed import Dict, List
 # 从全局配置中获取 Numba 类型
 from src.utils.constants import numba_config
 
-
 cache = numba_config["cache"]
-
 nb_float = numba_config["nb"]["float"]
 np_float = numba_config["np"]["float"]
 
 
-# 将此函数用 @njit 装饰，使其能被 Numba 编译
 @njit(cache=cache)
 def get_indicator_params():
     params = Dict.empty(
         key_type=types.unicode_type,
         value_type=nb_float,
     )
-    params["sma_period"] = float(14)
-    params["sma2_period"] = float(14)
-    params["bbands_period"] = float(14)
-    params["bbands_std_mult"] = float(2.0)
+    params["sma_period"] = nb_float(14)
+    params["sma2_period"] = nb_float(14)
+    params["bbands_period"] = nb_float(14)
+    params["bbands_std_mult"] = nb_float(2.0)
     return params
 
 
-# 将此函数用 @njit 装饰，使其能被 Numba 编译
 @njit(cache=cache)
 def get_backtest_params():
     params = Dict.empty(
         key_type=types.unicode_type,
         value_type=nb_float,
     )
-    params["signal_select"] = float(0)
-    params["atr_sl_mult"] = float(2.0)
+    params["signal_select"] = nb_float(0)
+    params["atr_sl_mult"] = nb_float(2.0)
     return params
 
 
-# 将主函数用 @njit 装饰
 @njit(cache=cache)
-def get_params_template(params_count):
-    """
-    生成回测所需的数据和参数。
-    整个函数都将在 Numba 中执行，从而实现高性能。
-    """
+def create_params_list_template(params_count):
     assert params_count > 0, "参数组合数量必须大于0"
-    # 在 JIT 编译的循环中调用 JIT 编译的函数
     indicator_params_list = List.empty_list(
         Dict.empty(
             key_type=types.unicode_type,
@@ -67,3 +57,118 @@ def get_params_template(params_count):
         backtest_params_list.append(get_backtest_params())
 
     return (indicator_params_list, backtest_params_list)
+
+
+@njit(cache=cache)
+def create_params_dict_template(params_count):
+    """
+    根据numba文档, 目前只支持List里面放Dict, 不支持Dict里面放List
+    所以,把List转成numpy数组解决问题
+    """
+
+    assert params_count > 0, "参数组合数量必须大于0"
+
+    indicator_params_dict = Dict.empty(
+        key_type=types.unicode_type,
+        value_type=nb_float[:],  # 使用 NumPy 数组类型
+    )
+
+    backtest_params_dict = Dict.empty(
+        key_type=types.unicode_type, value_type=nb_float[:]
+    )
+
+    params = get_indicator_params()
+    for key in params.keys():
+        # 直接创建 NumPy 数组，填充相同的值
+        arr = np.zeros(params_count, dtype=nb_float)
+        arr[:] = params[key]
+        indicator_params_dict[key] = arr
+
+    params = get_backtest_params()
+    for key in params.keys():
+        # 直接创建 NumPy 数组，填充相同的值
+        arr = np.zeros(params_count, dtype=nb_float)
+        arr[:] = params[key]
+        backtest_params_dict[key] = arr
+
+    return (indicator_params_dict, backtest_params_dict)
+
+
+@njit(cache=cache)
+def get_params_list_value(key, params_list):
+    params_count = len(params_list)
+    arr = np.zeros(params_count, dtype=nb_float)
+
+    for i in range(params_count):
+        arr[i] = params_list[i][key]
+    return arr
+
+
+@njit(cache=cache)
+def set_params_list_value(key, params_list, arr):
+    params_count = len(params_list)
+    assert params_count == len(arr), "更新数量应该和原始数量一致"
+
+    for i in range(params_count):
+        params_list[i][key] = arr[i]
+    return params_list
+
+
+@njit(cache=cache)
+def get_params_dict_value(key: str, params_dict):
+    return params_dict[key]
+
+
+@njit(cache=cache)
+def set_params_dict_value(key: str, params_dict, arr: np.ndarray):
+    first_key = ""
+    for k in params_dict.keys():
+        first_key = k
+        break
+
+    if first_key:
+        params_count = len(params_dict[first_key])
+        assert params_count == len(arr), "更新数量应该和原始数量一致"
+
+    params_dict[key] = arr
+    return params_dict
+
+
+@njit(cache=cache)
+def convert_params_dict_list(params_dict):
+    first_key = ""
+    for k in params_dict.keys():
+        first_key = k
+        break
+
+    # 检查字典是否为空
+    if not first_key:
+        return List.empty_list(
+            Dict.empty(
+                key_type=types.unicode_type,
+                value_type=nb_float,
+            )
+        )
+
+    params_count = len(params_dict[first_key])
+
+    for i in params_dict.keys():
+        assert params_count == len(params_dict[i]), "参数数量要彼此一致"
+
+    params_list = List.empty_list(
+        Dict.empty(
+            key_type=types.unicode_type,
+            value_type=nb_float,
+        )
+    )
+
+    for i in range(params_count):
+        params = Dict.empty(
+            key_type=types.unicode_type,
+            value_type=nb_float,
+        )
+        for key in params_dict.keys():
+            params[key] = params_dict[key][i]
+        params_list.append(params)
+
+    return params_list
